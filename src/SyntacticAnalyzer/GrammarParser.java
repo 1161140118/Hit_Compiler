878c5d306af1bad298bb8f70be047213b6bb061c
1161140118 <386506287@qq.com>
2019-04-18 20:33:57 +0800

    set start symbol

--- src/SyntacticAnalyzer/GrammarParser.java
+++ src/SyntacticAnalyzer/GrammarParser.java
@@ -18,8 +18,8 @@ import java.util.Set;
  */
 public class GrammarParser {
     /** 产生式 */
-    //Set<Production> productions = new HashSet<>();
-    public static final Map<String,List<Production>> productions = new HashMap<>();
+    // Set<Production> productions = new HashSet<>();
+    public static final Map<String, List<Production>> productions = new HashMap<>();
     /** 终结符集 */
     public static final Set<String> terminals = new HashSet<>();
     /** 非终结符集 */
@@ -28,25 +28,28 @@ public class GrammarParser {
     public static final Map<String, Set<String>> firstSet = new HashMap<>();
     /** 开始产生式 */
     public static Production START;
-    
-    
+    /** 开始符号 */
+    public static String startSymbol;
+
+
     public static void parseGrammar(String filepath) {
         setGrammerFromFile(filepath);
         setFirstSet();
         dropEmpty();
+        startSymbol = START.left;
     }
-    
+
     /**
      * delete $ in production.
      */
     private static void dropEmpty() {
-    	for (List<Production> list : productions.values()) {
-			for (Production production : list) {
-				production.right.remove("$");
-			}
-		}
+        for (List<Production> list : productions.values()) {
+            for (Production production : list) {
+                production.right.remove("$");
+            }
+        }
     }
-    
+
     private static void setFirstSet() {
         // 遍历终结符,firstSet 为自身
         for (String string : terminals) {
@@ -60,9 +63,9 @@ public class GrammarParser {
             firstSet.put(string, getFirst(string));
         }
     }
-    
-    private static Set<String> getFirst(String string){
-        if (firstSet.containsKey(string)) { 
+
+    private static Set<String> getFirst(String string) {
+        if (firstSet.containsKey(string)) {
             // 递归到终结符，或已确定的非终结符，停止
             return firstSet.get(string);
         }
@@ -81,33 +84,33 @@ public class GrammarParser {
         }
         return result;
     }
-    
+
     private static void setGrammerFromFile(String filepath) {
-    	boolean start= true;
+        boolean start = true;
         try {
             List<String> strings = Files.readAllLines(Paths.get(filepath));
             for (String string : strings) {
-                if (string.length()==0||string.charAt(0)=='#') {
+                if (string.length() == 0 || string.charAt(0) == '#') {
                     continue; // 空行或注释
                 }
                 Production production = new Production(string);
                 nonTerminals.add(production.left);
                 terminals.addAll(production.right);
-                
+
                 // 加入 productions
                 if (productions.keySet().contains(production.left)) {
                     productions.get(production.left).add(production);
-                }else {
+                } else {
                     List<Production> list = new ArrayList<>();
                     list.add(production);
-                    productions.put(production.left,list);
+                    productions.put(production.left, list);
                 }
-                
+
                 if (start) { // 设置开始产生式
-					START = production;
-					start=false;
-				}
-                
+                    START = production;
+                    start = false;
+                }
+
             }
             // 去除所有非终结符，得到终结符
             terminals.removeAll(nonTerminals);
@@ -115,11 +118,11 @@ public class GrammarParser {
             e.printStackTrace();
         }
     }
-    
+
     public static boolean isTerminal(String string) {
-    	return terminals.contains(string);
+        return terminals.contains(string);
     }
-    
+
 
 }
 
