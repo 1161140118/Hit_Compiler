2312b573be1fee328f5898a8502655008fb4fac1
1161140118 <386506287@qq.com>
2019-04-18 21:01:12 +0800

    refactor src and test

---
+++ test/LexAnalyzerTest.java
@@ -0,0 +1,19 @@
+import LexicalAnalyzer.LexAnalyzer;
+
+/**
+ * 
+ */
+
+/**
+ * @author standingby
+ *
+ */
+public class LexAnalyzerTest {
+    
+    public static void main(String[] args) {
+        new LexAnalyzer("test/code/source", "test/result");
+        
+    }
+
+
+}
---
+++ test/SynAnalyzerTest.java
@@ -0,0 +1,19 @@
+import SyntacticAnalyzer.SynAnalyzer;
+
+/**
+ * 
+ */
+
+/**
+ * @author standingby
+ *
+ */
+public class SynAnalyzerTest {
+
+
+    public static void main(String[] args) {
+        new SynAnalyzer("test/code/source", "test/grammar/grammar", "test/result");
+        
+    }
+
+}
