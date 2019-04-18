9bbf40fb966f3f4344e17065c3d2e182fa85f41f
1161140118 <386506287@qq.com>
2019-04-19 00:22:05 +0800

    add test

--- test/SynAnalyzerTest.java
+++ test/SynAnalyzerTest.java
@@ -10,9 +10,10 @@ import SyntacticAnalyzer.SynAnalyzer;
  */
 public class SynAnalyzerTest {
 
-
     public static void main(String[] args) {
-        new SynAnalyzer("test/code/source", "test/grammar/grammar", "test/result");
+//        new SynAnalyzer("test/code/source", "test/grammar/grammar", "test/result");
+//        new SynAnalyzer("test/code/hkcode","test/grammar/hkgram","test/result/hk");
+        new SynAnalyzer("test/code/arithcode","test/grammar/arithgram","test/result/arthi");
         
     }
 
