3a4e7ffec8344fed61c39e9875f734582e208491
1161140118 <386506287@qq.com>
2019-04-19 22:55:28 +0800

    new test

--- test/SynAnalyzerTest.java
+++ test/SynAnalyzerTest.java
@@ -13,6 +13,7 @@ public class SynAnalyzerTest {
     public static void main(String[] args) {
 //        new SynAnalyzer("test/code/source", "test/grammar/grammar", "test/result");
 //        new SynAnalyzer("test/code/hkcode","test/grammar/hkgram","test/result/hk");
+//        new SynAnalyzer("test/code/assigncode","test/grammar/assigngram","test/result/assign");
         new SynAnalyzer("test/code/arithcode","test/grammar/arithgram","test/result/arthi");
         
     }
